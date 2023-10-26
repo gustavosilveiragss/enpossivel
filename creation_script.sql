c0531b66c429f940430f6931a6d0e52a20524bd6
Gustavo Silveira <gustavo24gss@gmail.com>
2023-10-25 22:19:21 -0300

    timestamp now

--- creation_script.sql
+++ creation_script.sql
@@ -1,7 +1,7 @@
 -- Active: 1698186000660@@127.0.0.1@3306@enpossivel
 CREATE TABLE account (
     account_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name VARCHAR(255) NULL,
     email VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
@@ -10,7 +10,7 @@ CREATE TABLE account (
 
 CREATE TABLE product (
     product_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     img_path VARCHAR(255) NULL
@@ -18,14 +18,14 @@ CREATE TABLE product (
 
 CREATE TABLE cart (
     cart_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     account_id INT NOT NULL,
     FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
 
 CREATE TABLE cart_item (
     cart_item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     cart_id INT NOT NULL,
     product_id INT NOT NULL,
     FOREIGN KEY (cart_id) REFERENCES cart(cart_id),
@@ -34,20 +34,20 @@ CREATE TABLE cart_item (
 
 CREATE TABLE payment_method (
     payment_method_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `order` (
     order_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     account_id INT NOT NULL,
     FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
 
 CREATE TABLE order_item (
     order_item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES `order`(order_id),
--- mock_data.sql
+++ mock_data.sql
@@ -1,35 +1,35 @@
 -- Sample data for the User table
-INSERT INTO account (created_at, name, email, password, role) VALUES
-    ('2023-10-25 09:00:00', 'John Doe', 'john.doe@example.com', 'hashed_password', 'account'),
-    ('2023-10-25 10:30:00', 'Jane Smith', 'jane.smith@example.com', 'hashed_password', 'admin'),
-    ('2023-10-25 11:30:00', null, null, null, 'anon');
+INSERT INTO account (name, email, password, role) VALUES
+    ('John Doe', 'john.doe@example.com', 'hashed_password', 'account'),
+    ('Jane Smith', 'jane.smith@example.com', 'hashed_password', 'admin'),
+    (null, null, null, 'anon');
 
 -- Sample data for the Product table
-INSERT INTO product (created_at, title, price, img_path) VALUES
-    ('2023-10-25 08:45:00', 'Product A', 29.99, '/images/product_a.jpg'),
-    ('2023-10-25 09:15:00', 'Product B', 39.99, '/images/product_b.jpg');
+INSERT INTO product (title, price, img_path) VALUES
+    ('Product A', 29.99, '/images/product_a.jpg'),
+    ('Product B', 39.99, '/images/product_b.jpg');
 
 -- Sample data for the Cart table
-INSERT INTO cart (created_at, account_id) VALUES
-    ('2023-10-25 10:00:00', 1),
-    ('2023-10-25 11:30:00', 2);
+INSERT INTO cart (account_id) VALUES
+    (1),
+    (2);
 
 -- Sample data for the Cart_Item table
-INSERT INTO cart_item (created_at, cart_id, product_id, quantity) VALUES
-    ('2023-10-25 10:15:00', 1, 1, 2),
-    ('2023-10-25 10:30:00', 2, 2, 1);
+INSERT INTO cart_item (cart_id, product_id) VALUES
+    (1, 1),
+    (2, 2);
 
 -- Sample data for the Payment_Method table
-INSERT INTO payment_method (created_at, name) VALUES
-    ('2023-10-25 09:30:00', 'Credit Card'),
-    ('2023-10-25 09:45:00', 'Debit Card');
+INSERT INTO payment_method (name) VALUES
+    ('Credit Card'),
+    ('Debit Card');
 
 -- Sample data for the Order table
-INSERT INTO `order`(created_at, account_id) VALUES
-    ('2023-10-25 11:00:00', 1),
-    ('2023-10-25 12:30:00', 2);
+INSERT INTO `order`(account_id) VALUES
+    (1),
+    (2);
 
 -- Sample data for the Order_Item table
-INSERT INTO order_item (created_at, order_id, product_id, quantity) VALUES
-    ('2023-10-25 11:15:00', 1, 1, 2),
-    ('2023-10-25 11:30:00', 2, 2, 1);
+INSERT INTO order_item (order_id, product_id) VALUES
+    (1, 1),
+    (2, 2);
