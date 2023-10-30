5bac6a69d4109604eea9dbb4253e038d38fdcf86
vinicius <andrade.vinicius934@gmail.com>
2023-10-30 19:20:02 -0300

    cart/js: add an empty cart state looks way better than the empty table

--- js/cart.js
+++ js/cart.js
@@ -15,11 +15,19 @@ async function create_cart_table() {
 
     if (!response || !response.ok) return;
 
+    let cartTable = document.querySelector(".cart-table");
+    const cartIsEmpty = () => {
+        cartTable.innerHTML = "";
+        let cartTableWrapper = document.querySelector(".cart-table-wrapper");
+        cartTableWrapper.textContent = "O caldeirão está vazio!";
+    };
+
     const json = await response.json();
     if (json.length === 0) {
+        cartIsEmpty();
+        return;
     }
 
-    let cartTable = document.querySelector(".cart-table");
     let headerRow = document.createElement("tr");
     cartTable.appendChild(headerRow);
 
@@ -70,6 +78,11 @@ async function create_cart_table() {
             const new_quantity = Number(quantity.innerText) - 1;
             if (new_quantity === 0) {
                 cartTable.removeChild(cartRow);
+                // there will always be at least one row because of the headers
+                if (cartTable.rows.length === 1) {
+                    cartIsEmpty();
+                    return;
+                }
             } else {
                 quantity.innerText = new_quantity;
             }
