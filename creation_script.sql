0ac04dda1c7e3a5d8dda558867d7551b5fd5eba7
vinicius <andrade.vinicius934@gmail.com>
2023-11-01 21:44:27 -0300

    model: add `payment` and `payment_instalment`

--- creation_script.sql
+++ creation_script.sql
@@ -61,3 +61,21 @@ CREATE TABLE
         FOREIGN KEY (order_id) REFERENCES `order`(order_id),
         FOREIGN KEY (product_id) REFERENCES product(product_id)
     );
+
+CREATE TABLE
+    payment (
+        payment_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        order_id INT NOT NULL,
+        amount_payed DECIMAL(10, 2) NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES `order`(order_id),
+    )
+
+CREATE TABLE
+    payment_instalment (
+        payment_instalment_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        payment_id INT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        FOREIGN KEY (payment_id) REFERENCES payment(payment_id),
+    )
